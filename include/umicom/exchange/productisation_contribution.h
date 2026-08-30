f08cd8fd765ff143941f10e3e9bae05d1b89e240
Sammy Hegab <Sammy.Hegab@outlook.com>
2026-08-30 22:42:47 +0100

    feat(exchange): enhance productisation contribution

--- include/umicom/exchange/productisation_contribution.h
+++ include/umicom/exchange/productisation_contribution.h
@@ -16,7 +16,7 @@
 #ifndef UMICOM_EXCHANGE_PRODUCTISATION_CONTRIBUTION_H
 #define UMICOM_EXCHANGE_PRODUCTISATION_CONTRIBUTION_H
 
-#include "umicom/application/productisation/adoption.h"
+#include "umicom/application/productisation/session.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,6 +26,8 @@ const UmiProductApplicationAdoption *
 umi_exchange_productisation_contribution(void);
 UmiStatus umi_exchange_productisation_snapshot(
     UmiProductApplicationAdoptionSnapshot *out_snapshot);
+UmiStatus umi_exchange_product_session_init(
+    UmiProductApplicationSession *out_session);
 
 #ifdef __cplusplus
 }
